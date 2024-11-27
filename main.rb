2c360da7134a396d24a2bdab036cd9a9bb00cf4b
Edoardo De Piccoli <edoardodepiccoli2@gmail.com>
2024-11-27 15:22:34 +0100

    create single file with everything, add ruby-openai

--- main.rb
+++ main.rb
@@ -1,1 +1,11 @@
-puts 'script connected'
+require 'openai'
+
+client = OpenAI::Client.new(
+  access_token: 'access_token_goes_here',
+  log_errors: true # Highly recommended in development, so you can see what errors OpenAI is returning. Not recommended in production because it could leak private data to your logs.
+)
+
+file = File.new('./domande/tutto.tex')
+content = file.read
+
+sections = content.split('\section')
